767019d74d13aae35d4c07a61cca25fef785628a
Toni Simanjuntak <58927171+tonisimanjuntak@users.noreply.github.com>
2025-09-15 10:18:16 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -383,13 +383,14 @@ $config['encryption_key'] = 'SuPerEncKey2010';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session_serangkailah';
+$config['sess_samesite'] = 'Lax';
 $config['sess_expiration'] = 1800; //30 Menit (dalam detik)
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
+$config['sess_regenerate_destroy'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
